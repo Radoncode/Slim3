6e4b84750da749b61d203aa7581fa802a08ed25c
Radoncode <relbouni69@gmail.com>
2022-06-09 00:30:18 +0100

    using Middleware

--- public/index.php
+++ public/index.php
@@ -2,8 +2,18 @@
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App();
+class DemoMiddleware {
 
+    public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
+        $response->write('<h1>Bienvenue</h1>');
+        $response = $next($request, $response);
+        $response->write('<h1>Au revoir</h1>');
+        return $response;
+    }
+}
+
+$app = new \Slim\App();
+$app->add(new DemoMiddleware());
 
 $app->get('/salut/{nom}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
     return $response->write('hello ' . $args['nom']);
