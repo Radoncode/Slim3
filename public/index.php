9c09936aca5212d3149834179d019c3f7f1e97f4
Radoncode <relbouni69@gmail.com>
2022-06-09 03:07:13 +0100

    adding Middleware and database

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 require '../vendor/autoload.php';
 
+$pdo = new PDO('mysql:dbname=slim3;host=localhost','radoncode','root');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 class DemoMiddleware {
 
     public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
@@ -15,7 +18,11 @@ class DemoMiddleware {
 $app = new \Slim\App();
 $app->add(new DemoMiddleware());
 
-$app->get('/salut/{nom}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
+$app->get('/salut/{nom}', function (\Slim\Http\Request $request, \Slim\Http\Response $response, $args) use ($pdo){
+    $req = $pdo->prepare('SELECT * FROM posts');
+    $req->execute();
+    $posts = $req->fetchAll();
+    dump($posts);
     return $response->write('hello ' . $args['nom']);
 });
 
