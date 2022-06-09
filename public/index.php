910d515f5d47e04e149627b73e9f012a3f669efc
Radoncode <relbouni69@gmail.com>
2022-06-09 13:24:13 +0100

    adding cache, creation class container , first controller and first page twig

--- public/index.php
+++ public/index.php
@@ -1,67 +1,17 @@
 <?php
 
+use App\Controllers\PagesController;
+
 require '../vendor/autoload.php';
 
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true
+    ]
+]);
 
+require '../app/container.php';
 
-class DemoMiddleware {
-
-    public function __invoke(\Slim\Http\Request $request, \Slim\Http\Response $response, $next){
-        $response->write('<h1>Bienvenue</h1>');
-        $response = $next($request, $response);
-        $response->write('<h1>Au revoir</h1>');
-        return $response;
-    }
-}
-
-class Database {
-
-    private $pdo;
-
-    public function __construct(PDO $pdo){
-        $this->pdo = $pdo;
-    }
-
-    public function query($sql) {
-        $req = $this->pdo->prepare($sql);
-        $req->execute();
-        return $req->fetchAll();
-    }
-}
-
-class PagesController {
-
-    private $container;
-
-    public function __construct($container) {
-        $this->container = $container;
-    }
-
-    public function salut(\Slim\Http\Request $request, \Slim\Http\Response $response, $args){
-        $posts = $this->container->db->query('SELECT * FROM posts');
-        dump($posts);
-        return $response->write('hello ' . $args['nom']);
-    }
-}
-
-
-
-$app = new \Slim\App();
-$container = $app->getContainer();
-
-$container['pdo'] = function () {
-    $pdo = new PDO('mysql:dbname=slim3;host=localhost','radoncode','root');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-};
-
-$container['db'] = function ($container) {
-    return new Database($container->pdo);
-};
-
-$app->add(new DemoMiddleware());
-
-$app->get('/salut/{nom}', 'PagesController:salut');
+$app->get('/', PagesController::class . ':home');
 
 $app->run();
-
